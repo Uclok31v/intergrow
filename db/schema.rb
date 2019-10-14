e7f22b1792f46e4b0acca7a7999639dd029eab51
鵜戸西謙太郎 <ei.1px.9.01@gmail.com>
2019-10-14 21:03:01 +0900

    Add comment to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_14_110727) do
+ActiveRecord::Schema.define(version: 2019_10_14_115912) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "url"
+    t.text "comment"
     t.integer "count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
