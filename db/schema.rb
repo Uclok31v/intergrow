8f4ea40fc03e64bad4f3b977550ec5fefadcd8f7
鵜戸西謙太郎 <ei.1px.9.01@gmail.com>
2019-10-18 23:58:11 +0900

    Create user model and controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_17_134550) do
+ActiveRecord::Schema.define(version: 2019_10_18_144814) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", limit: 30, null: false
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 2019_10_17_134550) do
     t.integer "count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
