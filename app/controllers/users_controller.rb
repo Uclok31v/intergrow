7271721a8cc36346ce7c57b241c4f545d455f13a
鵜戸西謙太郎 <ei.1px.9.01@gmail.com>
2019-10-19 23:42:51 +0900

    Add user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,23 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to articles_url, notice: "「#{@user.name}」を投稿しました。"
+    else
+      render :new
+    end
   end
 
   def edit
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
+    end
 end
