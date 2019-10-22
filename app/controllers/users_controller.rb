251755a5e8e3813c612023857b2729af32c8261b
鵜戸西謙太郎 <ei.1px.9.01@gmail.com>
2019-10-22 22:13:01 +0900

    Add User editing function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,13 +14,23 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to articles_url, notice: "「#{@user.name}」を投稿しました。"
+      redirect_to articles_url, notice: "「#{@user.name}」を登録しました。"
     else
       render :new
     end
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to current_user, notice: "「#{@user.name}」を更新しました。"
+    else
+      render :edit
+    end
   end
 
   private
