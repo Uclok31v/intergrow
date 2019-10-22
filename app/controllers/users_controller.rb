0f437fc286396f7058f1148f88e066fe3d703df8
鵜戸西謙太郎 <ei.1px.9.01@gmail.com>
2019-10-22 22:36:05 +0900

    Add User deletion function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_url, notice: "「#{user.name}」を削除しました。"
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
