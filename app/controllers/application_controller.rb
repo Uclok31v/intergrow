cbd4af9a9aa5d9d11d3fef91a6b355c27006091a
鵜戸西謙太郎 <ei.1px.9.01@gmail.com>
2019-10-27 00:25:44 +0900

    Added article search function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
     include SessionsHelper
+
+    before_action :set_search
+    def set_search
+        @q = Article.all.order(created_at: :desc).ransack(params[:q])
+        @articles = @q.result(distinct: true)
+    end
+    
 end
